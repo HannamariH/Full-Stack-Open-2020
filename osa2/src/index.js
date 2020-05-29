95e2de9a77873b0e7045bbf153f652906f373378
Hannamari <hannamari.heiniluoma@gmail.com>
2020-05-29 11:20:33 +0300

    vaihe 2.5

--- osa2/src/index.js
+++ osa2/src/index.js
@@ -1,45 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-
-const Header = ({ course }) => {
-  return <h2>{course.name}</h2>
-}
-
-const Total = ({ course }) => {
-  const amounts = course.parts.map((part) => part.exercises)
-
-  const sum = amounts.reduce((acc, value) => acc + value)
-
-  return <h3>Number of exercises {sum}</h3>
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map((part) => (
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  )
-}
-
-const Course = (props) => {
-  return (
-    <div>
-      <Header course={props.course} />
-      <Content course={props.course} />
-      <Total course={props.course} />
-    </div>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
