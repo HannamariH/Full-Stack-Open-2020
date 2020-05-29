647507a45c853a427ea9f9d3b1c6f3610c61a69d
Hannamari <hannamari.heiniluoma@gmail.com>
2020-05-29 15:57:37 +0300

    vaihe 2.10

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -45,28 +45,57 @@ const App = () => {
 
   const personsToShow = showAll
     ? persons
-    : persons.filter(person => person.name.toUpperCase().includes(hakuehto.toUpperCase()))
+    : persons.filter((person) =>
+        person.name.toUpperCase().includes(hakuehto.toUpperCase())
+      )
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with  <input value={hakuehto} onChange={handleHakuehtoChange} />
-      </div>
-      <form onSubmit={onSubmit}>
-        <h2>Add a new</h2>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter value={hakuehto} onChange={handleHakuehtoChange} />
+      <h2>Add a new</h2>
+      <New
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        onSubmit={onSubmit}
+      />
       <h2>Numbers</h2>
-      {personsToShow.map((person) => (
+      <Persons personsToShow={personsToShow} />
+    </div>
+  )
+}
+
+const Filter = (props) => {
+  return (
+    <div>
+      filter shown with <input value={props.value} onChange={props.onChange} />
+    </div>
+  )
+}
+
+const New = (props) => {
+  return (
+    <form onSubmit={props.onSubmit}>
+      <div>
+        name: <input value={props.newName} onChange={props.handleNameChange} />
+      </div>
+      <div>
+        number:{" "}
+        <input value={props.newNumber} onChange={props.handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = (props) => {
+  return (
+    <div>
+      {props.personsToShow.map((person) => (
         <p key={person.name}>
           {person.name} {person.number}
         </p>
