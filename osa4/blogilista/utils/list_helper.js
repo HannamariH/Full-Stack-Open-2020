33c8478a6a488c93d3e434f4838bebeebee9363a
Hannamari <hannamari.heiniluoma@gmail.com>
2020-08-28 15:04:01 +0300

    tehtävä 4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
     return 1
   }
+
+const totalLikes = (array) => {
+    const reducer = (sum, item) => {
+        const likes = item.likes
+        return sum + likes
+    }
+    return array.reduce(reducer, 0)
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
