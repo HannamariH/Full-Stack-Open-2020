bce3f5f8607b3a3b3ec180a3b00df17220da1b54
Hannamari <hannamari.heiniluoma@gmail.com>
2020-10-30 14:05:10 +0200

    vaihe 5.15

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -16,7 +16,7 @@ describe("Blog tests: ", () => {
 
     beforeEach(() => {
         component = render(
-            <Blog blog={blog} buttonLabel="view"/>
+            <Blog blog={blog} buttonLabel="view" />
         )
     })
 
@@ -36,6 +36,30 @@ describe("Blog tests: ", () => {
         const div = component.container.querySelector(".hiddenFirst")
         expect(div).not.toHaveStyle("display: none")
     })
+})
+
+test("when like button is clicked twice, mock function is also called twice", () => {
+
+    const blog= {
+        title: "Christmas blog",
+        author: "Maija",
+        url: "christmasblog.com",
+        likes: 5
+    }
+
+    const mockHandler = jest.fn()
+
+    
+    const component = render(<Blog blog={blog} buttonLabel="view" handleLike={mockHandler}/>)   
+
+    const button = component.getByText("view")
+    fireEvent.click(button)
+
+    const likeButton = component.getByText("like")
+    fireEvent.click(likeButton)
+    fireEvent.click(likeButton)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
 
 })
 
